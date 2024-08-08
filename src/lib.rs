d12a43a9cc045a8a24de0c724808e0bd4712dac8
Kavin-Nithil <rknit2002@gmail.com>
2024-08-08 20:43:00 +0530

    chore: Added tests for search

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,37 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> { // dyn is short for dynamic
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("Contents: \n {:?}", contents);
+    // println!("Contents: \n {:?}", contents);
+    for line in search(&config.query, &contents){
+        println!("{:?}", line)
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query:&str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "uct";
+        let contents = "\
+Rust:
+safe, fast, productive
+Pick three:.";
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
 }
