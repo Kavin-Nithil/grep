7c1c2ab6ac213173b87a8067a812cc8ecefa66eb
Kavin-Nithil <rknit2002@gmail.com>
2024-08-08 19:26:33 +0530

    Chore: Added poem.txt and logic to get arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
-    println!("Hello, world!");
+
+    let config = parse_config(&args);
+    let query = config.query;
+    let file_path = config.file_path;
+
+    println!("In file {:?}", query);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text = {:?}", &contents);
+    dbg!(&args);
+    println!("{query:?}");
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config {query, file_path}
 }
