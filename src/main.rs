da89fb4df38738df63e10f49869a19dde1f64576
Kavin-Nithil <rknit2002@gmail.com>
2024-08-08 20:15:27 +0530

    ref: added run function which does the operation

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
-    let query = config.query;
-    let file_path = config.file_path;
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing args: {err}");
+        process::exit(1);
+    });
 
-    println!("In file {:?}", query);
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    println!("With text = {:?}", &contents);
-    dbg!(&args);
-    println!("{query:?}");
+    if let Err(e) = run(config) {
+        println!(" Encountered the following error {:?}", e);
+        process::exit(1);
+    };
 }
 
 struct Config {
@@ -38,4 +38,29 @@ impl Config {
             file_path
         }
     }
+
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments.");
+        } else if args.len() > 3 {
+            return Err("Extra arguments provided");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config {
+            query,
+            file_path
+        })
+    }
+}
+
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> { // dyn is short for dynamic
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("Contents: \n {:?}", contents);
+
+    Ok(())
 }
