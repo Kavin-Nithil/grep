7c4f20a5e57472b9e0ed90bc7ddbcc009481dd29
Kavin-Nithil <rknit2002@gmail.com>
2024-08-08 19:36:24 +0530

    Ref: Removed parse config function and added a constructor for config

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     let query = config.query;
     let file_path = config.file_path;
 
@@ -20,6 +20,18 @@ fn main() {
 struct Config {
     query: String,
     file_path: String
+}
+
+impl Config {
+    fn new(args: &String) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config {
+            query,
+            file_path
+        }
+    }
 }
 
 fn parse_config(args: &[String]) -> Config {
